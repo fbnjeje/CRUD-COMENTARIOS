b239c544cb7ad6a80ffa3867d7f232b2bdbe6923
Neonx <fabiangomes2015@gmail.com>
2023-08-10 17:01:17 -0500

    first commit

---
+++ src/app/Components/list-comentarios/list-comentarios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-comentarios',
+  templateUrl: './list-comentarios.component.html',
+  styleUrls: ['./list-comentarios.component.css']
+})
+export class ListComentariosComponent {
+
+}
---
+++ src/app/Components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  img = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png"
+}
